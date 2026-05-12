b7c2bbaefd684fe43fa7662a910ce8b06d5cc616
Nennayz <nennay@gmail.com>
2026-05-12 18:12:44 -0500

    test: assert pm.name in test_load_slay_hack

--- tests/test_project_loader.py
+++ tests/test_project_loader.py
@@ -6,6 +6,7 @@ from models.content_job import PMProfile
 def test_load_slay_hack():
     pm = load_project("slay_hack")
     assert isinstance(pm, PMProfile)
+    assert pm.name == "Slay"
     assert pm.page_name == "Slay Hack Agency"
     assert pm.brand.nora_max_retries == 2
     assert "#D4AF37" in pm.brand.visual.colors
